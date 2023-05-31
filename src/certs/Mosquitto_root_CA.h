ab230344827ab9b95a1d731fc4a0a95da8b00516
Jun Wen <130400462+bootcampiot@users.noreply.github.com>
2023-05-31 10:19:48 -0700

    Update Mosquitto_root_CA.h

--- src/certs/Mosquitto_root_CA.h
+++ src/certs/Mosquitto_root_CA.h
@@ -1,3 +1,5 @@
+#include <pgmspace.h>
+
 static const char root_CA[] PROGMEM = R"EOF(
 -----BEGIN CERTIFICATE-----
 // Mosquitto certificate authority file (mosquitto.org.crt (PEM format), which is downloaded from https://test.mosquitto.org/ssl/mosquitto.org.crt
