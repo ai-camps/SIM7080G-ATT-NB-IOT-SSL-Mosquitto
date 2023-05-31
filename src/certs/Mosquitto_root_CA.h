1279cefd08913f07b303a3eb6b935ecd6e6dfd70
Jun Wen <130400462+bootcampiot@users.noreply.github.com>
2023-05-31 10:18:40 -0700

    Create Mosquitto_root_CA.h

---
+++ src/certs/Mosquitto_root_CA.h
@@ -0,0 +1,5 @@
+static const char root_CA[] PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+// Mosquitto certificate authority file (mosquitto.org.crt (PEM format), which is downloaded from https://test.mosquitto.org/ssl/mosquitto.org.crt
+-----END CERTIFICATE-----
+)EOF";
